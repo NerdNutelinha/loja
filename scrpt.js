51f9221bd6a64d867fe51d3305ca43be400aa91f
NerdNutelinha <geovanyalangomes15@gmail.com>
2026-05-28 05:27:55 -0300

    Update scrpt.js

--- scrpt.js
+++ scrpt.js
@@ -1,5 +1,9 @@
 function mostrarMensagem(){
-
-    document.getElementById("mensagem").innerHTML =
-    "O ambiente da liderança é utilizado para armazenar arquivos, convites e relatórios de forma organizada e segura.";
+    var msgElement = document.getElementById("mensagem");
+    
+    // Altera o texto
+    msgElement.innerHTML = "O ambiente da liderança é utilizado para armazenar arquivos, convites e relatórios de forma organizada e segura.";
+    
+    // Faz a caixinha estilizada aparecer na tela
+    msgElement.style.display = "block";
 }
