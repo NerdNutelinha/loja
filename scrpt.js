f30bf2cf4fbf7b337157dab0d5c425653a48a3e2
NerdNutelinha <geovanyalangomes15@gmail.com>
2026-05-28 05:20:53 -0300

    Create scrpt.js

---
+++ scrpt.js
@@ -0,0 +1,5 @@
+function mostrarMensagem(){
+
+    document.getElementById("mensagem").innerHTML =
+    "O ambiente da liderança é utilizado para armazenar arquivos, convites e relatórios de forma organizada e segura.";
+}
